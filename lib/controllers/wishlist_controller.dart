6d56cd0c1aa074ea2be8a325aea262c95448efeb
Ehsan Computers <muhammadmaaz4405@gmail.com>
2026-08-28 16:56:56 +0500

    wishlis_controller work is going

---
+++ lib/controllers/wishlist_controller.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+import '../models/product_model.dart';
+
+class WishlistController extends GetxController{
+  final wishlistProducts = <ProductModel>[].obs;
+
+  // add / remove product
+  void toggleWishlist(ProductModel product){
+    final alreadyExists = wishlistProducts.any(
+        (item) => item.id == product.id,
+    );
+
+    if(alreadyExists) {
+      wishlistProducts.removeWhere(
+          (item) => item.id == product.id,
+      );
+    } else {
+
+    }
+
+  }
+}
